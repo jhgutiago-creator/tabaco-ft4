8168ff808a5f35c1cfdbb5ed7990ffa12cd84147
jhgutiago-creator <jhgutiago@gmail.com>
2025-08-25 02:26:00 -0300

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,7 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar'
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import { useFrameworkReady } from '@/hooks/useFrameworkReady'import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
-  useFrameworkReady();
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
